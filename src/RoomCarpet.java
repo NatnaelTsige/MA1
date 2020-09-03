ab10e5a3c4916533112f7bfbd7389c4579ceeab8
NatnaelTsige <70252612+NatnaelTsige@users.noreply.github.com>
2020-09-03 02:07:42 -0700

    RoomCarpet crated. Fields declared.

--- src/RoomCarpet.java
+++ src/RoomCarpet.java
@@ -1,3 +1,5 @@
 public class RoomCarpet {
-
+    // Fields for dimension and cost of carpet per square ft (cPSF) declared but not initialized.
+    private RoomDimension dimension;
+    private double cPSF;
 }
