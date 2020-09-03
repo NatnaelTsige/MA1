ff729219c056678257894b09726d81453573e20d
NatnaelTsige <70252612+NatnaelTsige@users.noreply.github.com>
2020-09-03 13:10:23 -0700

    public removed.

--- src/RoomCarpet.java
+++ src/RoomCarpet.java
@@ -1,4 +1,4 @@
-public class RoomCarpet {
+class RoomCarpet {
     // Fields for dimension and cost of carpet per square ft (cPSF) declared but not initialized.
     private RoomDimension dimension;
     private double cPSF;
