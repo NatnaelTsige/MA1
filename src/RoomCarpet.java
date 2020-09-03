265a95690a2593027cc4f8fa895587f0733b9b64
NatnaelTsige <70252612+NatnaelTsige@users.noreply.github.com>
2020-09-03 12:22:00 -0700

    RoomCarpet completed.

--- src/RoomCarpet.java
+++ src/RoomCarpet.java
@@ -2,4 +2,22 @@ public class RoomCarpet {
     // Fields for dimension and cost of carpet per square ft (cPSF) declared but not initialized.
     private RoomDimension dimension;
     private double cPSF;
+
+
+    public RoomCarpet(RoomDimension dimension, double cPSF) {
+        super();
+        this.dimension = dimension;
+        this.cPSF= cPSF;
+    }
+
+    public double getTotalCost(){
+        return dimension.getArea()*cPSF;
+    }
+
+    public String toString()
+    {
+        String console = dimension + "\nsquare ft carpet cost: $" + getTotalCost();
+        return console;
+    }
+
 }
