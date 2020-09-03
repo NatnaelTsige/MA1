1557213508e14e35d85e6bdbbb70fb9f6e441e89
NatnaelTsige <70252612+NatnaelTsige@users.noreply.github.com>
2020-09-03 01:59:37 -0700

    RoomDimension completed.

--- src/RoomDimension.java
+++ src/RoomDimension.java
@@ -1,7 +1,11 @@
-public class RoomDimension {
-   //fields for the length and width declared but not initialized.
-   double length;
-   double width;
+class RoomDimension {
+   //Fields for the length and width declared but not initialized.
+   private double length;
+   private double width;
 
-
+   //Instance method.
+   //Returns the area.
+   public double area(){
+      return length * width;
+   }
 }
